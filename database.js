2827789ea3ae0c23d131ab4d507d5f41431af9d2
whatsapp1232 <jannettelenx@gmail.com>
2025-01-27 09:29:53 -0800

    Updated database.js to use sqlite3 and added better error handling

--- database.js
+++ database.js
@@ -1,18 +1,26 @@
-const Database = require('better-sqlite3');
+const sqlite3 = require('sqlite3').verbose();
 
 // Veritabanı dosyasını açıyoruz (veritabanı yoksa oluşturulacak)
-const db = new Database('./url_shortener.db', { verbose: console.log });
-
-// URL tablosunu oluşturuyoruz
-db.prepare(`
-    CREATE TABLE IF NOT EXISTS links (
+const db = new sqlite3.Database('./url_shortener.db', (err) => {
+  if (err) {
+    console.error("Veritabanı bağlantı hatası: " + err.message);
+  } else {
+    console.log('Veritabanına bağlandı');
+    db.run(`
+      CREATE TABLE IF NOT EXISTS links (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         short_url TEXT NOT NULL UNIQUE,
         target_url TEXT NOT NULL
-    )
-`).run();
-
-console.log('Veritabanı ve tablo oluşturuldu veya zaten mevcut');
+      )
+    `, (err) => {
+       if (err) {
+         console.error("Tablo oluşturma hatası: " + err.message);
+       } else {
+        console.log("Tablo oluşturuldu veya zaten mevcut");
+       }
+     });
+   }
+});
 
 // Veritabanı bağlantısını dışa aktarıyoruz
 module.exports = db;
